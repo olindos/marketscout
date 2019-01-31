11493daccd1a2d08aa562843d0ccbe8b71f87fb2
olindos <yason4ik87@gmail.com>
2019-01-31 12:10:03 +0200

    Config capistarno for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "marketscout"
+set :repo_url, "https://github.com/olindos/marketscout"
+
+set :deploy_to, '/home/ms/marketscout'
+
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,1 @@
+server '18.216.58.135', user: 'ms', roles: %w{app db web}
